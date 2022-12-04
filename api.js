e497a6e663ed925f6308262054ab12b9bda6882f
Juan Chanaga <juan.chanaga@artemisa.tech>
2022-12-04 01:30:08 -0500

    Agrega metodo post

--- api.js
+++ api.js
@@ -3,6 +3,7 @@ var express = require("express");
 var router = express.Router();
 var query = "mongodb+srv://juanchanaga:jpchr90@cluster0.hueohbz.mongodb.net/taskBD?retryWrites=true&w=majority"
 const db = (query);
+var TaskModel = require('./task_schema');
 
 mongoose.Promise = global.Promise;
 
@@ -15,6 +16,29 @@ mongoose.connect(db, {
     } else {
         console.log("Se ha conectado con la base de datos exitosamente");
     }
+});
+
+router.post('/create-task', function (req, res) {
+    let task_id = req.body.TaskId;
+    let name = req.body.Name;
+    let deadline = req.body.Deadline;
+
+    let task = {
+        TaskId: task_id,
+        Name: name,
+        Deadline: deadline
+    }
+    var newTask = new TaskModel(task);
+
+    newTask.save(function (err, data) {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Internal error\n");
+        }
+        else {
+            res.status(200).send("OK\n");
+        }
+    });
 });
 
 module.exports = router;
